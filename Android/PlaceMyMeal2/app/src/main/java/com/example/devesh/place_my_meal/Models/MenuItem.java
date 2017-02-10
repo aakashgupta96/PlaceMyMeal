b58b814b6a38dba7e2e9973f53add4c4456c75f4
Devesh <deveshsaini96@gmail.com>
2017-02-10 23:08:57 +0530

    Offline Database

--- Android/PlaceMyMeal2/app/src/main/java/com/example/devesh/place_my_meal/Models/MenuItem.java
+++ Android/PlaceMyMeal2/app/src/main/java/com/example/devesh/place_my_meal/Models/MenuItem.java
@@ -6,6 +6,50 @@ package com.example.devesh.place_my_meal.Models;
 
 public class MenuItem {
 
+    public static class Food{
+        Integer MenuID;
+        Integer CompanyID;
+        Integer Quantity;
+        Integer Value;
 
+        public Food(Integer menuID, Integer companyID, Integer quantity, Integer value) {
+            MenuID = menuID;
+            CompanyID = companyID;
+            Quantity = quantity;
+            Value = value;
+        }
+
+        public Integer getMenuID() {
+            return MenuID;
+        }
+
+        public void setMenuID(Integer menuID) {
+            MenuID = menuID;
+        }
+
+        public Integer getCompanyID() {
+            return CompanyID;
+        }
+
+        public void setCompanyID(Integer companyID) {
+            CompanyID = companyID;
+        }
+
+        public Integer getQuantity() {
+            return Quantity;
+        }
+
+        public void setQuantity(Integer quantity) {
+            Quantity = quantity;
+        }
+
+        public Integer getValue() {
+            return Value;
+        }
+
+        public void setValue(Integer value) {
+            Value = value;
+        }
+    }
 
 }
---
+++ Android/PlaceMyMeal2/app/src/main/java/com/example/devesh/place_my_meal/Models/Table.java
@@ -0,0 +1,20 @@
+package com.example.devesh.place_my_meal.Models;
+
+/**
+ * Created by devesh on 10/2/17.
+ */
+
+public class Table {
+
+
+
+    static String COMMA = " , ";
+    static String LBR = " ( ";
+    static String RBR =" ) ";
+
+    static String TYPE_INT =" INTEGER ";
+    static String TYPE_TEXT = " TEXT ";
+    static String TYPE_REAL = " REAL ";
+    static String TYPE_INT_PK = " INTEGER PRIMARY KEY ";
+
+}

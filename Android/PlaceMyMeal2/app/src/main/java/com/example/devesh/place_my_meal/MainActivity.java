3096d7180e7b5e8eca6dcb1fbf0877a69998faef
Harshil <harshilyadav@rocketmail.com>
2017-02-10 20:29:03 +0530

    harshil login

--- Android/PlaceMyMeal2/app/src/main/java/com/example/devesh/place_my_meal/MainActivity.java
+++ Android/PlaceMyMeal2/app/src/main/java/com/example/devesh/place_my_meal/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.devesh.place_my_meal;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private String mUsername;
+    private GoogleApiClient mGoogleApiClient;
+    TextView UserName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        UserName=(TextView)findViewById(R.id.username);
         setContentView(R.layout.activity_main);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, SignIn.class));
+            finish();
+            return;
+        } else {
+            mUsername = mFirebaseUser.getDisplayName();
+            UserName.setText(mUsername);
+        }
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+//                .addApi(Auth.GOOGLE_SIGN_IN_API)
+//                .build();
+
     }
+    private void logout(){
+        mFirebaseAuth.signOut();
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+        mFirebaseUser = null;
+        mUsername = "ANONYMOUS";
+        startActivity(new Intent(this, SignIn.class));
+    }
+
+
+
 }

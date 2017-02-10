516e86b7f2ec2a5cd8628e435c26dc5d66cf31fc
Devesh <deveshsaini96@gmail.com>
2017-02-10 23:41:41 +0530

    #4

--- Android/PlaceMyMeal2/app/src/main/java/com/example/devesh/place_my_meal/OrderPage.java
+++ Android/PlaceMyMeal2/app/src/main/java/com/example/devesh/place_my_meal/OrderPage.java
@@ -2,8 +2,21 @@ package com.example.devesh.place_my_meal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import com.example.devesh.place_my_meal.Models.MenuItem;
+
+import java.util.ArrayList;
 
 public class OrderPage extends AppCompatActivity {
+
+    ListView myListView;
+    ArrayList <MenuItem.Food> myList;
+    Integer TotalBill;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,4 +26,38 @@ public class OrderPage extends AppCompatActivity {
 
 
     }
+
+    public class Adapter extends BaseAdapter{
+
+        ArrayList<MenuItem.Food> mList;
+
+        public Adapter(ArrayList<MenuItem.Food> mList) {
+            this.mList = mList;
+        }
+
+        @Override
+        public int getCount() {
+            return mList.size();
+        }
+
+        @Override
+        public MenuItem.Food getItem(int i) {
+            return mList.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+
+
+
+            return view;
+        }
+    }
+
 }
